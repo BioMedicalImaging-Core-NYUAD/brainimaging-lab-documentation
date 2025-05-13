b7df1839293bbb74e2ea1bb52db6ba03f9d6c4b8
Hzaatiti <hadizaatiti@gmail.com>
2025-05-13 11:38:26 +0400

    fix rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,13 +26,15 @@ master_doc = "index"
 # -- General configuration
 
 extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
     "sphinxcontrib.mermaid",
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
-    'sphinx_rtd_theme',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx_rtd_theme",
     
 ]
 
@@ -43,10 +45,12 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
+
+html_static_path = ['_static']
 
 html_css_files = [
     "custom.css",
@@ -54,37 +58,41 @@ html_css_files = [
 
 
 html_theme_options = {
+    "logo_only": False,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
      "style_external_links": False,
     "vcs_pageview_mode": "",
     "style_nav_header_background": "#561A70",
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
     # Toc options
 }
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 html_logo = "graphic/NYU_Logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
-# -- Theme options ---------------------------------------------------------
-html_theme_options = {
-    'navigation_depth': 4,
-    'titles_only': False,
-    'logo_only': False,
-}
+
+
 
 # -- Math options ---------------------------------------------------------
 mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
