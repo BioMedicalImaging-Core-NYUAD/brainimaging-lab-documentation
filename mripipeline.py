2095d1baf13ae1269c930b8074a12e2287f6ac74
Hzaatiti <hadizaatiti@gmail.com>
2025-05-19 18:24:37 +0400

    add eeg-fmri to brainimaging lab

---
+++ mripipeline.py
@@ -0,0 +1,55 @@
+"""
+mripipeline - Python base package for MRI data post-processing.
+"""
+
+__version__ = "0.1.0"
+
+
+class MRIpipeline():
+    """Primary class for MRI pipeline."""
+#Example of method docstring
+
+# def template_function(kind=None):
+#     """
+#     Return a list of random ingredients as strings.
+#
+#     :param kind: Optional "kind" of ingredients.
+#     :type kind: list[str] or None
+#     :raise megpipeline.InvalidKindError: If the kind is invalid.
+#     :return: The ingredients list.
+#     :rtype: list[str]
+#     """
+#     return ["shells", "gorgonzola", "parsley"]
+
+    def __init__(self):
+        return None
+
+
+    def get_raw_data(self, file_name):
+        """
+        Return a list of random as strings.
+
+        :param kind: Optional "kind" .
+        :type kind: list[str] or None
+        :return: the converted data list.
+        :rtype: list[str]
+        """
+        return None
+def get_raw_data(self, file_name):
+    """
+    Return a list of random as strings.
+
+    :param kind: Optional "kind" .
+    :type kind: list[str] or None
+    :return: the converted data list.
+    :rtype: list[str]
+    """
+    return None
+
+if __name__ == "__main__":
+
+    pipeline = MEGpipeline()
+
+    pipeline.get_raw_data('MEG_DATA/')
+
+    print("test")
