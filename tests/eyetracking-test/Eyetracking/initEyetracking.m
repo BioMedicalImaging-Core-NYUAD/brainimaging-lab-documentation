d42104762b0f186e65131b17132375078f58409a
putiw <pw1246@nyu.edu>
2025-11-03 16:32:14 +0400

    not working

--- tests/eyetracking-test/Eyetracking/initEyetracking.m
+++
@@ -1,9 +0,0 @@
-function EL = initEyetracking(const, w_ptr)
-
-%% Initialize Eyetracker
-
-[EL, exitFlag] = initEyelinkStates('eyestart', w_ptr, {const.eyeFileName, const});
-if exitFlag, return, end
-
-EL.eyeDataDir = const.eyeDataDir;
-EL.eyeFile = const.eyeFileName;
