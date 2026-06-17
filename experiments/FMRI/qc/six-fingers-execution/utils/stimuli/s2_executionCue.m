0cc3279635373014d775aa95f58063e362531996
putiw <pw1246@nyu.edu>
2026-06-17 15:10:57 +0400

    add new task

--- experiments/FMRI/qc/six-fingers-execution/utils/stimuli/s2_executionCue.m
+++ experiments/FMRI/qc/six-fingers-execution/utils/stimuli/s2_executionCue.m
@@ -1,5 +1,5 @@
 function [pa, exitFlag] = s2_executionCue(VP, pa, kb, experimentStartTime, blockIdx, trialInBlock, fingerName)
-% S2_EXECUTIONCUE - Present one highlighted finger execution cue.
+% S2_EXECUTIONCUE - Present one highlighted finger cue (flexing or tapping).
 
 imagePath = fullfile(pa.imageDir, pa.imageFiles(fingerName));
 dmRow = zeros(1, numel(pa.fingerNames));
@@ -7,6 +7,6 @@ fingerIdx = find(strcmp(pa.fingerNames, fingerName), 1);
 dmRow(fingerIdx) = 1;
 
 [pa, exitFlag] = present_image_epoch(VP, pa, kb, experimentStartTime, ...
-    imagePath, 'execution', fingerName, blockIdx, trialInBlock, pa.stimulusDuration, dmRow);
+    imagePath, pa.taskType, fingerName, blockIdx, trialInBlock, pa.stimulusDuration, dmRow);
 
 end
